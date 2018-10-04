550d1de67e37db7f78752649bc6b7c2a3d3d2139
Samuel Cornet <samuel@noukod.com>
2018-10-04 14:42:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,19 @@
-class Song
-  attr_accessor :name, :artist
+class Song 
+attr_accessor :name, :artist 
 
-  @@all = []
+def initialize(name)
+  @name = name 
+end 
 
-  def initialize(name, artist = nil)
-    @name = name
-    @artist = artist
-    @@all << self
-    # return nil if !artist.include?(song)
-  end
-end
+def self.new_by_filename(filename)
+  song = self.new(filename.split(" - ")[1])
+  song.artist_name = (filename.split(" - ")[0])
+  song 
+end 
+
+def artist_name=(name)
+  self.artist = Artist.find_or_create_by_name(name)
+  artist.add_song(self)
+end 
+
+end 
