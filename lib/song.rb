2dfd04a45667e42af787c9b97ce3d2d95add9928
Samuel Cornet <samuel@noukod.com>
2018-10-04 14:37:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,6 +13,12 @@ end
 def self.artist
   self.artist.name
 end
+def self.new_by_filename(filename)
+    song = self.new
+    song.title = filename.split(" - ")[1]
+    song
+end
+
 # def artist_name=(name)
 #   self.artist = Artist.find_or_create_by_name(name)
 #   artist.add_song(self)
